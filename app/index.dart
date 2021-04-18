110497150d4e6c33133e02feaeadea7c131d31d4
DESKTOP-0P848CA\edinl_6z7c6t <edinliu@gmail.com>
2021-04-18 20:20:38 +0800

    不使用http動詞

--- app/index.dart
+++ app/index.dart
@@ -7,27 +7,18 @@ String _host = InternetAddress.loopbackIPv4.host;
 Future main() async {
   var server = await HttpServer.bind(_host, 4049);
   await for (var req in server) {
-    ContentType contentType = req.headers.contentType;
     HttpResponse response = req.response;
-
-    if (req.method == 'POST' &&
-        contentType?.mimeType == 'application/json' /*1*/) {
-      try {
-        String content = await utf8.decoder.bind(req).join(); /*2*/
-        var data = jsonDecode(content) as Map; /*3*/
-        var res = await getResult(data);
-        req.response
-          ..statusCode = HttpStatus.ok
-          ..write('${res}');
-      } catch (e) {
-        response
-          ..statusCode = HttpStatus.internalServerError
-          ..write('Exception during file I/O: $e.');
-      }
-    } else {
+    try {
+      String content = await utf8.decoder.bind(req).join(); /*2*/
+      var data = jsonDecode(content) as Map; /*3*/
+      var res = await getResult(data);
+      req.response
+        ..statusCode = HttpStatus.ok
+        ..write('${res}');
+    } catch (e) {
       response
-        ..statusCode = HttpStatus.methodNotAllowed
-        ..write('Unsupported request: ${req.method}.');
+        ..statusCode = HttpStatus.internalServerError
+        ..write('Exception during file I/O: $e.');
     }
     await response.close();
   }
